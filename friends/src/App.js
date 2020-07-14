eb329dc9506546387b546a70c4953243520f079e
Amy Davis <amydavis@Blakes-MacBook-Pro-2.local>
2020-07-14 17:33:41 -0400

    Finished PrivateRoute component, implenting to App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import LoginPage from './Components/LoginPage';
+import PrivateRoute from './Components/PrivateRoute'
 
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
        <Link to="/login">Login</Link>
        <Link to="/protected">Protected Page</Link>
        <Switch>
+         <PrivateRoute exact path="/protected" component/>
          <Route path="/login" component={LoginPage} />
          <Route component={LoginPage} />
        </Switch>
